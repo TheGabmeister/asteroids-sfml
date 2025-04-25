c657c3c593e9b8bdad8c5f43cfe550e66a9c291f
TheGabmeister <thegabmeister@protonmail.com>
2025-04-26 07:35:35 +0800

    Load player image

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,17 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+    static sf::Texture texture;
+    static bool loaded = false;
+    static std::unique_ptr<sf::Sprite> sprite;
+    if (!loaded) {
+        if (!texture.loadFromFile(RESOURCES_PATH "image.png")) {
+            // Handle error (optional)
+        }
+        sprite = std::make_unique<sf::Sprite>(texture);
+        loaded = true;
+    }
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -16,6 +27,17 @@ int main()
         }
 
         window.clear();
+
+
+        if (sprite) {
+            window.draw(*sprite);
+        }
+
         window.display();
     }
+}
+
+void Initialize()
+{
+
 }
