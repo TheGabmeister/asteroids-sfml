99ce836d0f6655226c09ca0d24cba55c04722f4e
TheGabmeister <thegabmeister@protonmail.com>
2025-04-26 09:11:37 +0800

    Add player movement controls

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({600u, 800u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
 
+    const sf::Vector2u windowDimensions{600u, 800u};
+    auto window = sf::RenderWindow(sf::VideoMode(windowDimensions), "Asteroids");
+    window.setFramerateLimit(144);
 
     // Initialize
     /////////////////////////////////////////////////////////////////////////////////////
@@ -15,15 +16,15 @@ int main()
         window.getSize().x / 2.f,
         window.getSize().y / 2.f
     };
-    
+    sf::Vector2f newPosition = playerPosition;
+    const float speed = 3.0f;
+
     sf::Texture texture;
     if (!texture.loadFromFile(RESOURCES_PATH "image.png")) {
         throw std::runtime_error("Failed to load image.png");
     }
-    sf::Sprite sprite(texture);
-    //float spriteLength = sprite.getGlobalBounds().getCenter().x
-    //std::cout << "Sprite length: " << spriteLength << std::endl;
-    sprite.setOrigin(sprite.getGlobalBounds().getCenter());
+    sf::Sprite playerSprite(texture);
+    playerSprite.setOrigin(playerSprite.getGlobalBounds().getCenter());
     
 
     // Game Loop
@@ -38,14 +39,40 @@ int main()
             }
         }
 
-        sprite.setPosition(playerPosition); 
+
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            newPosition.x = playerPosition.x - speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            newPosition.x = playerPosition.x + speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            newPosition.y = playerPosition.y - speed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            newPosition.y = playerPosition.y + speed;
+        }
+
+        // Check if out of bounds
+        if (newPosition.x < 0 || newPosition.x > windowDimensions.x)
+        {
+            newPosition.x = playerPosition.x;
+        }
+        if (newPosition.y < 0 || newPosition.y > windowDimensions.y)
+        {
+            newPosition.y = playerPosition.y;
+        }
+
+        playerPosition = newPosition;
+        playerSprite.setPosition(playerPosition); 
 
 
         // DRAW
         ////////////////////////////////////////////////////////////////////////////////////
         window.clear();
 
-        window.draw(sprite);
+        window.draw(playerSprite);
 
 
         window.display();
